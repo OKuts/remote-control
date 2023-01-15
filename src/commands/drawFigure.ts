38114f38f60970178862c8eddac117a856d403aa
Ol_Kuts <kuts.ol.vas@gmail.com>
2023-01-15 04:03:51 +0200

    fix: overflow last command menu

--- src/commands/drawFigure.ts
+++ src/commands/drawFigure.ts
@@ -1,31 +1,27 @@
-import {Button, down, left, mouse, Point, right, up} from '@nut-tree/nut-js'
+import {Button, mouse } from '@nut-tree/nut-js'
 
 export const drawFigure = async (figure: string, px1: string, px2 = '') => {
   const value = Number(px1)
   const value2 = px2 ? Number(px2) : value
+  mouse.config.mouseSpeed = 50
+  const point = await mouse.getPosition()
   if (figure === 'circle') {
-    mouse.config.autoDelayMs = 50
-    mouse.config.mouseSpeed = 500
     const point = await mouse.getPosition()
     for (let i = 0; i <= Math.PI; i += 0.1) {
-      if (i > 0) {
-        await mouse.pressButton(Button.LEFT)
-      }
+      if (i > 0 && i < 0.2) await mouse.pressButton(Button.LEFT)
       const newPoint = {x: point.x + (value * Math.cos(i)), y: point.y + (value * Math.sin(i))}
       await mouse.move([newPoint])
     }
     for (let i = 0; i <= Math.PI; i += 0.1) {
-      await mouse.pressButton(Button.LEFT)
       const newPoint = {x: point.x - (value * Math.cos(i)), y: point.y - (value * Math.sin(i))}
       await mouse.move([newPoint])
     }
-  } else {
-    await mouse.releaseButton(Button.LEFT)
+   } else {
     await mouse.pressButton(Button.LEFT)
-    await mouse.move(right(value))
-    await mouse.move(down(value2))
-    await mouse.move(left(value))
-    await mouse.move(up(value2))
+    await mouse.move([{x: point.x + value, y: point.y}])
+    await mouse.move([{x: point.x + value, y: point.y + value2}])
+    await mouse.move([{x: point.x, y: point.y + value2}])
+    await mouse.move([{x: point.x, y: point.y}])
   }
   await mouse.releaseButton(Button.LEFT)
 }
